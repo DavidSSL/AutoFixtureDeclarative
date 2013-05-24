5f9ac93881d285a3669957d75afb1d9337f15171
David Siew <davidsiew@outlook.com>
2013-05-24 09:50:43 +0100

    - Imperative implementation of "Dealing With Types Without Public Constructors" for basic example i.e. the interface implementation does not take any parameter.

---
+++ Example.UnitTests/TypesWithoutPublicCtrsTests.cs
@@ -0,0 +1,20 @@
+﻿using Examples;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace Example.UnitTests
+{
+    // Based on http://blog.ploeh.dk/2009/04/23/DealingWithTypesWithoutPublicConstructors/
+    public class TypesWithoutPublicCtrsTests
+    {
+        [Fact]
+        public void SomeTest()
+        {
+            var fixture = new Fixture();
+
+            fixture.Register<IMyInterface>(() => new FakeMyInterface());
+
+            var sut = fixture.Create<TypesWithoutPublicCtrs>();
+        }
+    }
+}
---
+++ Examples/TypesWithoutPublicCtrs.cs
@@ -0,0 +1,12 @@
+﻿namespace Examples
+{
+    public class TypesWithoutPublicCtrs
+    {
+        private readonly IMyInterface _mi;
+
+        public TypesWithoutPublicCtrs(IMyInterface mi)
+        {
+            _mi = mi;
+        }
+    }
+}
