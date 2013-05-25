f90af8b5e7bae0b2ab6845a44dd9798d9a76cd59
David Siew <davidsiew@outlook.com>
2013-05-25 11:27:48 +0100

    - Implement http://blog.ploeh.dk/2009/06/01/SettingPropertyValuesWhileBuildingAnonymousVariablesWithAutoFixture/ i.e. setting properties explicitly while creating anonymous variable imperatively.

---
+++ Example.UnitTests/SettingPropertyValuesWhileBuildingAnonymousVariablesTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FluentAssertions;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace Example.UnitTests
+{
+    public class SettingPropertyValuesWhileBuildingAnonymousVariablesTests
+    {
+        [Fact]
+        public void ImperativeStyle()
+        {
+            // Fixture setup
+            var fixture = new Fixture();
+            var min = fixture.Create<int>();
+            var max = min + 1;
+
+            var sut = fixture.Build<Filter>()
+                             .With(s => s.Max, max)
+                             .With(s => s.Min, min)
+                             .Create();
+
+            // Exercise system
+            
+            // Verify outcome
+            sut.Max.Should().BeGreaterThan(sut.Min);
+            // Fixture teardown
+        }
+    }
+
+    public class Filter
+    {
+        private int _min;
+        public int Max { get; set; }
+
+        public int Min
+        {
+            get { return _min; }
+
+            set
+            {
+
+                if (value > Max)
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                _min = value;
+            }
+        }
+    }
+}
---
+++ Examples/Filter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Examples
+{
+    public class Filter
+    {
+        private int _min;
+        public int Max { get; set; }
+
+        public int Min
+        {
+            get { return _min; }
+
+            set
+            {
+
+                if (value > Max)
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                _min = value;
+            }
+        }
+    }
+}
